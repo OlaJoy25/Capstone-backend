1ea6c52589341706e84a2cc7c1ac17c46adcd31f
OlaJoy25 <olajoycelyn.ak@gmail.com>
2024-03-26 18:22:59 +0200

    update

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const cors = require('cors');
-
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://jola:If5JC8E8PUuFuNzr@cluster0.bqef0uk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  "mongodb+srv://jola:If5JC8E8PUuFuNzr@cluster0.bqef0uk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 // Check for MongoDB connection
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
 // Define a simple Mongoose schema and model
-const SampleSchema = new mongoose.Schema({
-  name: String,
+const SubscriberSchema = new mongoose.Schema({
+  email: String,
 });
 
-const SampleModel = mongoose.model('Sample', SampleSchema);
+const subscriberModel = mongoose.model("subscriber", SubscriberSchema);
 
 // Define a route to create and retrieve a sample document
-app.get('/', async (req, res) => {
-  // Create a sample document
-  const sampleDocument = new SampleModel({ name: 'Sample Name' });
-  await sampleDocument.save();
+app.get("/", async (req, res) => {
+  return res.send("Hello World");
+});
 
-  // Retrieve all sample documents
-  const samples = await SampleModel.find();
-
-  res.json(samples);
+// /subscribe route to create a new subscriber
+app.post("/subscribe", async (req, res) => {
+  const { email } = req.body;
+  const subscriber = new subscriberModel({ email });
+  await subscriber.save();
+  return res.send(subscriber._id);
 });
 
 app.listen(port, () => {
-  console.log('Server is running at http://localhost:' +port);
+  console.log("Server is running at http://localhost:" + port);
 });
